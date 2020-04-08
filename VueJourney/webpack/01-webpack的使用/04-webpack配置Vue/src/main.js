be90cb46d78b67c1c3c54dadd83b16a1d21e58af
Sun, Guanjun <guanjun.sun@capgemini.com>
2020-04-08 23:46:42 +0800

    dev: Webpack plugin

--- VueJourney/webpack/01-webpack的使用/04-webpack配置Vue/src/main.js
+++ VueJourney/webpack/01-webpack的使用/04-webpack配置Vue/src/main.js
@@ -20,15 +20,14 @@ document.writeln("加油，sunflower");
 
 //导入Vue
 import Vue from "vue";
+//import App from './vue/app.js'//app.js导出的是默认对象，所以可以任意命名
+import App from "./vue/App.vue";
+
 //创建vue实例
 new Vue({
   el: "#app",
-  template: `
-  <div>
-    <h2>{{message}}</h2> 
-  </div>
-  `,
-  data: {
-    message: "hello webpack",
+  template: "<App/>", //这里会将index.html中的div替换掉, app本身就是根，不需要加div
+  components: {
+    App,
   },
 });
---
+++ VueJourney/webpack/01-webpack的使用/05-webpack配置Plugin/src/vue/Cpn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>我是一个component</h2>
+    {{name}}
+  </div>
+</template>
+<script>
+export default {
+  name: "Cpn",
+  data() {
+    return {
+      name: "我是一个组件的name"
+    };
+  }
+};
+</script>
+<style lang="stylus" scoped></style>
