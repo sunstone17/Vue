4c3efcd709930dfad8068eaf48683eb4896d868b
Sun, Guanjun <623115450@qq.com>
2020-04-12 23:04:23 +0800

    DEV: add tabbar project

---
+++ VueJourney/VueRouter/02-tabbar/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <tab-bar></tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from './components/tabbar/TabBar'
+export default {
+  name: "App",
+  components:{
+    TabBar
+  }
+};
+</script>
+
+<style>
+/* 引入css文件，需要用@import */
+@import "./assets/css/base.css";
+</style>
