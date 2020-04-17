3cfd401eab05a85f20583a189e2afa6616346a16
Sun, Guanjun <623115450@qq.com>
2020-04-17 23:01:48 +0800

    DEV: update Vuex

---
+++ VueJourney/Vuex/testvuex/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    App--------------
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementBy5">+5</button>
+    <button @click="addStudent">+Stu</button>
+    <br />App mutations info--------------
+    <h2>{{$store.state.info}}</h2>
+    <button @click="updateInfo">update info</button>
+
+    <br />app getter----------------
+    <h2>{{$store.getters.more60Stus}}</h2>
+    <h2>{{$store.getters.more60StusLength}}</h2>
+    <h2>{{$store.getters.moreAgeStus(100)}}</h2>HelloWorld-------
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import store from "./store";
+import { INCREMENT } from "./store/mutation-types";
+
+export default {
+  name: "App",
+  store,
+  components: {
+    HelloWorld
+  },
+  methods: {
+    increment() {
+      this.$store.commit(INCREMENT);
+    },
+    decrement() {
+      this.$store.commit("decrement");
+    },
+    incrementBy5() {
+      this.$store.commit("incrementBy", { count: 5 });
+    },
+    addStudent() {
+      const stu = { id: 1000, name: "添加一个", age: 100 };
+      this.$store.commit("addStudent", stu);
+    },
+    updateInfo(){
+      this.$store.commit("updateInfo");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ VueJourney/Vuex/testvuex/src/store/index.js
@@ -0,0 +1,61 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import { INCREMENT } from "./mutation-types";//命名导入
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    counter: 1000,
+    students: [
+      { id: 10, name: "hhe", age: 199 },
+      { id: 11, name: "haha", age: 10 },
+      { id: 22, name: "babe", age: 88 },
+      { id: 33, name: "niuniu", age: 77 },
+      { id: 55, name: "huahua", age: 66 }
+    ],
+    info: {
+      age: 100,
+      name: "kobe",
+      height: 1.88
+    }
+  },
+  mutations: {
+    [INCREMENT](state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    incrementBy(state, payload) {
+      state.counter += payload.count;
+    },
+    addStudent(state, payload) {
+      state.students.push(payload);
+    },
+    updateInfo(state) {
+      Vue.set(state.info, "id", 1000); //响应式
+      Vue.delete(state.info, "age"); //响应式
+      state.info["bigbang"] = "bangbangbang"; //不是响应式
+    }
+  },
+  actions: {},
+  getters: {
+    more60Stus(state) {
+      //第一个参数默认为state
+      return state.students.filter(x => x.age > 60);
+    },
+    more60StusLength(state, getters) {
+      ////第二个参数默认为getters
+      return getters.more60Stus.length;
+    },
+    moreAgeStus(state, getters) {
+      //这里只能传入2个参数
+      //返回一个函数，外部当作一个函数调用
+      return age => {
+        return state.students.filter(x => x.age > age);
+      };
+    }
+  },
+  modules: {}
+});
+export default store;
