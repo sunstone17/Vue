7726cf9e8d8d5b885e7f9b7050fd33a16e4c2e21
Sun, Guanjun <623115450@qq.com>
2020-04-18 22:57:16 +0800

    DEV: nav-bar

--- VueJourney/supermall/src/App.vue
+++ VueJourney/supermall/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
 
+import MainTabBar from 'components/content/maintabbar/MainTabBar'
 export default {
   name: 'App',
   components: {
+    MainTabBar
   }
 }
 </script>
