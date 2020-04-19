fe453d5397e2c672eaa89146f45d31a56076cb9e
Sun, Guanjun <623115450@qq.com>
2020-04-19 19:59:46 +0800

    DEV: update mall project

---
+++ VueJourney/supermall/src/network/request.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import {baseUrl} from './const'
+
+export function request(config) {
+  const instance = axios.create({
+    baseURL:baseUrl,
+    timeout: 5000
+  });
+
+  instance.interceptors.request.use(
+    config => {
+      if(config.url.indexOf("?") > 0){
+        config.url += "&version=2020"
+      }else{
+        config.url += "?version=2020"
+      }
+      return config;
+    },
+    err => {
+      console.log(err);
+    }
+  );
+  instance.interceptors.response.use(result=>{
+    return result.data;//返回所有结果
+  }, err=>{
+    console.log(err)
+  })
+  return instance(config); //这里就是一个promise
+}
--- VueJourney/supermall/src/views/home/Home.vue
+++ VueJourney/supermall/src/views/home/Home.vue
@@ -3,23 +3,80 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <HomeSwiper :banners="banners"></HomeSwiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款', '精选']"></tab-control>
+    <ul>
+      <li v-for="item in num">{{item}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+//child
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+//common
 import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabcontrol/TabControl";
+//js
+import { getHomeMultidata } from "network/home.js";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      keywords: [],
+      dKeyworkd: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        news: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      num: 50
+    };
+  },
   components: {
-    NavBar
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl
+  },
+  created() {
+    getHomeMultidata()
+      .then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+        this.keywords = res.data.keywords;
+        this.dKeyworkd = res.data.dKeyworkd;
+      })
+      .catch(err => {});
   }
 };
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
