4b8f8c4d6249bfce6bc14830b43a181f32c178d9
Sun, Guanjun <623115450@qq.com>
2020-06-06 13:03:37 +0800

    DEV: update vue plugin

--- VueJourney/supermall/src/main.js
+++ VueJourney/supermall/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import toast from 'components/common/toast'
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
@@ -10,6 +12,14 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue();
 // 安装toast插件
 Vue.use(toast)
+
+// 解决移动端300ms延迟的问题
+FastClick.attach(document.body)
+
+// 使用图片懒加载
+Vue.use(VueLazyLoad, {
+  loading: require("./assets/img/common/placeholder.png")
+});
 
 new Vue({
   render: h => h(App),
